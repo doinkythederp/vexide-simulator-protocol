dcf92af0010ffcc2194e5a56b8291027f406a3a9
doinkythederp <doinkythederp@icloud.com>
2024-05-15 11:15:34 -0700

    feat: add controllers

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use std::num::NonZeroU16;
+use serde::{Deserialize, Serialize};
+use std::{num::NonZeroU16, path::PathBuf};
 
+#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum Event {
     ScreenDraw {
         command: DrawCommand,
@@ -44,13 +46,16 @@ pub enum Event {
     },
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum Command {
     Touch {
         pos: Point,
         event: TouchEvent,
     },
     ControllerUpdate(ControllerUpdate),
-    USD(USD),
+    USD {
+        root: Option<PathBuf>,
+    },
     VEXLinkOpened {
         port: SmartPort,
         mode: LinkMode,
@@ -72,10 +77,24 @@ pub enum Command {
         voltage: f64,
     },
     StartExecution,
+    SetBatteryCapacity {
+        capacity: f64,
+    },
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[non_exhaustive]
 pub struct VCodeSig;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[non_exhaustive]
+pub enum Device {}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[non_exhaustive]
+pub struct RobotState;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum DrawCommand {
     Fill {
         shape: Shape,
@@ -91,6 +110,7 @@ pub enum DrawCommand {
     },
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum Shape {
     Rectangle {
         top_left: Point,
@@ -105,11 +125,13 @@ pub enum Shape {
     },
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct Point {
-    x: i16,
-    y: i16,
+    pub x: i16,
+    pub y: i16,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum DeviceStatus {
     Motor {
         velocity: f64,
@@ -125,56 +147,109 @@ pub enum DeviceStatus {
     },
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum MotorGearset {
     Red,
     Green,
     Blue,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum MotorBrakeMode {
     Coast,
     Brake,
     Hold,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[non_exhaustive]
 pub struct MotorFlags;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum LinkMode {
     Manager,
     Worker,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum TouchEvent {
     Released,
     Pressed,
     Held,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum Port {
     Smart(SmartPort),
     Adi(AdiPort),
 }
 
-pub struct SmartPort(u8);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct SmartPort(pub u8);
 
-pub struct AdiPort(u8);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct AdiPort(pub u8);
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum CompMode {
     Auto,
     Driver,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct Color {
-    r: u8,
-    g: u8,
-    b: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum LogLevel {
     Trace,
     Info,
     Warn,
     Error,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Serialize, Deserialize)]
+pub struct Battery {
+    pub voltage: f64,
+    pub current: f64,
+    pub capacity: f64,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+pub enum ControllerUpdate {
+    /// Implementors can send raw controller state to the simulator,
+    /// allowing for keyboard-and-mouse-based control.
+    Raw(ControllerState),
+    /// Or, they can send the UUID of a physical controller (more efficient and allows for SDL2 mappings).
+    UUID(String),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct ControllerState {
+    pub axis1: i32,
+    pub axis2: i32,
+    pub axis3: i32,
+    pub axis4: i32,
+    pub button_l1: bool,
+    pub button_l2: bool,
+    pub button_r1: bool,
+    pub button_r2: bool,
+    pub button_up: bool,
+    pub button_down: bool,
+    pub button_left: bool,
+    pub button_right: bool,
+    pub button_x: bool,
+    pub button_b: bool,
+    pub button_y: bool,
+    pub button_a: bool,
+    pub button_sel: bool,
+    pub battery_level: i32,
+    pub button_all: bool,
+    pub flags: i32,
+    pub battery_capacity: i32,
 }
 
 // #[cfg(test)]
